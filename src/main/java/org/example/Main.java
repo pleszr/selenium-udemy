fc22d780addffc08b5d9049e41658b24574fb4ee
rplesz <plesz.roland@gmail.com>
2024-06-05 22:22:49 +0200

    NOTICKET sector 5 lesson 35

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,24 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         WebDriver driver = new ChromeDriver();
-        driver.close();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("roland");
+        driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+        driver.findElement(By.className("signInBtn")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        //driver.close();
+
+
     }
 }
