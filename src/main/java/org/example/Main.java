ce437365756dd50d67880bb301f86050e6f709d4
rplesz <plesz.roland@gmail.com>
2024-06-05 17:03:31 +0200

    NOTICKET initial creation

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        WebDriver driver = new ChromeDriver();
+        driver.close();
+    }
+}
